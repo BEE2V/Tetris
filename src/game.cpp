d2b4bec84c44d7124b478588a7143a75498b80cb
BEE2V <135147675+BEE2V@users.noreply.github.com>
2025-12-26 22:23:48 +0530

    Rotation Done

--- src/game.cpp
+++ src/game.cpp
@@ -46,6 +46,9 @@ void Game::HandleInput()
     case KEY_DOWN:
         MoveBlockDown();
         break;
+    case KEY_UP:
+        RotateBlock();
+        break;
     default:
         break;
     }
@@ -90,4 +93,13 @@ bool Game::IsBlockOutside()
     }
 
     return false;
+}
+
+void Game::RotateBlock()
+{
+    currentBlock.Rotate();
+    if (IsBlockOutside())
+    {
+        currentBlock.UndoRotation();
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <raylib.h>
 #include "game.h"
 
+double lastUpdateTime = 0;
+
+bool EventTriggered(double interval)
+{
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime > interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
 int main()
 {
     Color darkBlue = {44, 44, 127, 255};
@@ -17,6 +30,10 @@ int main()
         game.HandleInput();
 
         // 2. Updating State
+        if (EventTriggered(0.2))
+        {
+            game.MoveBlockDown();
+        }
 
         // 3. Drawing
         BeginDrawing();
